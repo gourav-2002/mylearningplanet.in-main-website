2a84a7f6c090975132384349a89b5d8737fe2791
gourav-2002 <gour9205.av81@gmail.com>
2026-04-02 16:55:32 +0530

    add google anayltics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import './globals.css'
@@ -16,6 +17,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XL70DZ253W"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XL70DZ253W');
+          `}
+        </Script>
         <Header />
         <main className="min-h-screen bg-gray-50 font-sans flex flex-col">
           {children}
